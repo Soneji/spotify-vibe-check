4f0dd392ecf2b4b54397c1f0d85f58580a8384ae
Dhaval Soneji <dhaval@soneji.xyz>
2020-06-29 19:42:42 +0100

    make pretty

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var client_id = process.env.client_id; // Your client id
 var client_secret = process.env.client_secret; // Your secret
 
 // change the if statement to your server's hostname and your redirect url you've set in spotify developer settings
-if (os.hostname() == 'soneji.xyz'){
+if (os.hostname() == 'soneji.xyz') {
   var redirect_uri = 'https://spotify-vibe-check.soneji.xyz/callback'
 } else {
   var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
@@ -31,7 +31,7 @@ if (os.hostname() == 'soneji.xyz'){
  * @param  {number} length The length of the string
  * @return {string} The generated string
  */
-var generateRandomString = function(length) {
+var generateRandomString = function (length) {
   var text = '';
   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
@@ -46,10 +46,10 @@ var stateKey = 'spotify_auth_state';
 var app = express();
 
 app.use(express.static(__dirname + '/public'))
-   .use(cors())
-   .use(cookieParser());
+  .use(cors())
+  .use(cookieParser());
 
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
 
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
@@ -66,7 +66,7 @@ app.get('/login', function(req, res) {
     }));
 });
 
-app.get('/callback', function(req, res) {
+app.get('/callback', function (req, res) {
 
   // your application requests refresh and access tokens
   // after checking the state parameter
@@ -95,18 +95,18 @@ app.get('/callback', function(req, res) {
       json: true
     };
 
-    request.post(authOptions, function(error, response, body) {
+    request.post(authOptions, function (error, response, body) {
       if (!error && response.statusCode === 200) {
 
         var access_token = body.access_token,
-            refresh_token = body.refresh_token;
-        
+          refresh_token = body.refresh_token;
+
         // make logs pretty
         var today = new Date();
         var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();  
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
         console.log('=========================================================')
-        console.log(date + ' ' + time);      
+        console.log(date + ' ' + time);
 
         var options = {
           url: 'https://api.spotify.com/v1/me',
@@ -114,7 +114,7 @@ app.get('/callback', function(req, res) {
           json: true
         };
         // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
+        request.get(options, function (error, response, body) {
           console.log(body);
         });
 
@@ -133,7 +133,7 @@ app.get('/callback', function(req, res) {
   }
 });
 
-app.get('/refresh_token', function(req, res) {
+app.get('/refresh_token', function (req, res) {
 
   // requesting access token from refresh token
   var refresh_token = req.query.refresh_token;
@@ -147,7 +147,7 @@ app.get('/refresh_token', function(req, res) {
     json: true
   };
 
-  request.post(authOptions, function(error, response, body) {
+  request.post(authOptions, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token;
       res.send({
--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
-(function() {
-    
+(function () {
+
     /**
     * Obtains parameters from the hash of the URL
     * @return Object
@@ -7,14 +7,14 @@
     function getHashParams() {
         var hashParams = {};
         var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
+            q = window.location.hash.substring(1);
         while (e = r.exec(q)) {
             hashParams[e[1]] = decodeURIComponent(e[2]);
         }
         return hashParams;
     }
-    
-    
+
+
     function getTracks(playlist_id) {
         //shubh.js
         $.ajax({
@@ -25,7 +25,7 @@
             async: false,
             success: function (response) {
                 response.items.forEach(track => {
-                    let n=track.track.name.toLowerCase();
+                    let n = track.track.name.toLowerCase();
                     if (
                         n.includes('krishna') ||
                         n.includes('krsna') ||
@@ -34,7 +34,7 @@
                         n.includes('hanuman') ||
                         n.includes('shiv') ||
                         n.includes('govinda')
-                    ){
+                    ) {
                         // console.log(n);
                         return true;
                     }
@@ -44,13 +44,13 @@
         });
         return false;
     }
-    
+
     var params = getHashParams();
-    
+
     var access_token = params.access_token,
-    refresh_token = params.refresh_token,
-    error = params.error;
-    
+        refresh_token = params.refresh_token,
+        error = params.error;
+
     if (error) {
         alert('There was an error during the authentication');
     } else {
@@ -63,10 +63,10 @@
                 headers: {
                     'Authorization': 'Bearer ' + access_token
                 },
-                success: function(response) {
+                success: function (response) {
                     response.items.forEach(playlist => {
                         setTimeout(() => {
-                            
+
                             $.ajax({
                                 url: playlist.tracks.href,
                                 headers: {
@@ -77,10 +77,10 @@
                                     const arrAvg = arr => arr.reduce((a, b) => a + b, 0) / arr.length;
                                     function getRandom(arr, n) {
                                         var result = new Array(n),
-                                        len = arr.length,
-                                        taken = new Array(len);
+                                            len = arr.length,
+                                            taken = new Array(len);
                                         if (n > len)
-                                        throw new RangeError("getRandom: more elements taken than available");
+                                            throw new RangeError("getRandom: more elements taken than available");
                                         while (n--) {
                                             var x = Math.floor(Math.random() * len);
                                             result[n] = arr[x in taken ? taken[x] : x];
@@ -88,17 +88,17 @@
                                         }
                                         return result;
                                     }
-                                    let energies=[],dancabilities=[],vibes=[],ids=[];
+                                    let energies = [], dancabilities = [], vibes = [], ids = [];
                                     response.items.forEach(item => {
                                         try {
-                                            ids.push(item.track.id);                                        
+                                            ids.push(item.track.id);
                                         } catch {
 
                                         }
                                     });
-                                    if(response.items.length > 100){
-                                        ids=getRandom(ids,100);
-                                    } 
+                                    if (response.items.length > 100) {
+                                        ids = getRandom(ids, 100);
+                                    }
                                     $.ajax({
                                         url: `https://api.spotify.com/v1/audio-features?ids=${ids.join()}`,
                                         headers: {
@@ -107,7 +107,7 @@
                                         async: false,
                                         success: function (response) {
                                             response.audio_features.forEach(features => {
-                                                try{
+                                                try {
                                                     energies.push(features.energy);
                                                     dancabilities.push(features.danceability);
                                                     vibes.push(features.valence);
@@ -119,13 +119,13 @@
                                     });
                                     let a = arrAvg(energies); a = a * 100; a = a.toFixed(2); energies = [];
                                     let b = arrAvg(dancabilities); b = b * 100; b = b.toFixed(2); dancabilities = [];
-                                    let c = arrAvg(vibes); c=c*100; c=c.toFixed(2); vibes = [];
-                                    if (c < 50){
+                                    let c = arrAvg(vibes); c = c * 100; c = c.toFixed(2); vibes = [];
+                                    if (c < 50) {
                                         d = `${c}%<br>big sad%`;
                                     } else {
                                         d = `${c}%<br>big happy`;
                                     }
-                                    let n=playlist.name.toLowerCase();
+                                    let n = playlist.name.toLowerCase();
                                     if (
                                         n.includes('krishna') ||
                                         n.includes('krsna') ||
@@ -141,7 +141,7 @@
                                     ) {
                                         d = `${c}%<br>big Haribol!`;
                                     }
-                                
+
                                     // if ( getTracks(playlist.id)==true ){
                                     //     d = `Hare Krsna<br>(${c}%)`;
                                     // }
@@ -154,31 +154,31 @@
                                             <td>${b}%</td>
                                             <td>${d}</td>
                                             </tr>`
-                                            );
-                                        } catch (error) {
-                                            $('table').append(
-                                                `<tr class="animate bounceInDown">
+                                        );
+                                    } catch (error) {
+                                        $('table').append(
+                                            `<tr class="animate bounceInDown">
                                                 <td><img width=60 src=${playlist.images[0].url}></td>
                                                 <td>${playlist.name}</td>
                                                 <td>${a}%</td>
                                                 <td>${b}%</td>
                                                 <td>${d}</td>
                                                 </tr>`
-                                                );
-                                        }
-                                        $('table').css("max-width","100%");
+                                        );
                                     }
-                                });
-                            },1000);
-                        });
-                    }
-                });
-            } else {
-                // render initial screen
-                $('#login').show();
-                $('#loggedin').hide();                
-            }
+                                    $('table').css("max-width", "100%");
+                                }
+                            });
+                        }, 1000);
+                    });
+                }
+            });
+        } else {
+            // render initial screen
+            $('#login').show();
+            $('#loggedin').hide();
         }
+    }
 
     document.getElementById('obtain-new-token').addEventListener('click', function () {
         $.ajax({
@@ -196,4 +196,3 @@
     }, false);
 
 })();
-        
